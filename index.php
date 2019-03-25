b6644dc60938148f685923c680315c1da4eb5759
Mayur Kathale <mayur.k@acommerce.asia>
2019-03-25 15:52:20 +0700

    updating api url

--- index.php
+++ index.php
@@ -24,7 +24,8 @@ if(count($text) < 3) {
     } else {
 	$values = $env[$text[1]][$text[0]];
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, 'http://mayurkathale.com:8080/job/'.$values["job_name"].'/buildWithParameters?token='.$values["token"].'&BRANCH='.$text[1]);
+	curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_URL, 'http://'.getenv("JENKINS_USER").':'.getenv("JENKINS_TOKEN").'@mayurkathale.com:8080/job/'.$values["job_name"].'/buildWithParameters?token='.$values["token"].'&BRANCH='.$text[1]);
         $result = curl_exec($curl);
         curl_close($curl);
         $client->send('Deploying command');
